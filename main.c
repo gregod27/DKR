43d55a9e01c1c8ccae8ff10ffbf0014265531c65
gregod27 <114358827+gregod27@users.noreply.github.com>
2023-01-16 00:39:21 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,130 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-int i = 0, j = 0, sum_line = 0,N,**A,**B,value;
-int sumering(int *line,int N);
-void sorting(int **B, int N);
-void printMatrix(int **matrix,int N);
-int main()
+
+
+int pow(int a, int N);
+int main ()
 {
-    printf("Enter the square matriix number, N= ");
+    int N = 0;
+    float S = 1 ,a = 0;
+    printf("N: ");
     scanf("%d", &N);
+    printf("a: ");
+    scanf("%f", &a);
 
-    A=(int**)malloc(N*sizeof(int*));
-
-    printf("\nEnter matrix elements values NxN= %d:\n\n", N*N);
-
-    for(i=0; i<N; i++)
-    {
-        A[i]=(int*)malloc(N*sizeof(int));
-
-        if(A[i]==NULL)
-        {
-            printf("Error allocate memory A2.\n");
-            for(i=0; i<N; i++)
-            {
-                free(A[i]);
-                free(A);
-                exit(1);
-            }
+    for(int n=0; n<=N; n++)
+        if(a+n != 0){
+            S = S + pow(-1, n)*(pow(a, n))/(a+n);
         }
 
-        for(j=0; j<N; j++)
-        {
-            printf("A[%d][%d]= ", i, j);
-            scanf("%d", &value);
-            A[i][j]= value;
-        }
-    }
+    printf("S: %f",S);
+    getch();
+    return 0;
 
-    B=(int**)malloc(N*sizeof(int*));
-
-    if(B==NULL)
-    {
-        printf("Error allocate memory B1.\n");
-        exit(1);
-    }
-
-    for(i=0; i<N; i++)
-    {
-        B[i]=(int*)malloc(value*sizeof(int));
-
-        if(B[i]==NULL)
-        {
-            printf("Error allocate memory B2.\n");
-            for(i=0; i<N; i++)
-            {
-                free(B[i]);
-                free(B);
-                exit(1);
-            }
-        }
-
-        for(j=0; j<N; j++)
-        {
-            B[i][j]=A[i][j];
-        };
-    }
-    printf("\nA Matrix:\n");
-    printMatrix(A,N);
-    printf("\nB Matrix:\n");
-    sorting(B,N);
 }
-
-int sumering(int *line,int N)
+int pow(int a, int N)
 {
-    int sum = 0;
-    for (i=0; i<N; i++)
+    int value=1;
+    if(N==0)
     {
-        sum +=line[i];
+        return 1;
     }
-    return sum;
-}
-
-void sorting(int **B, int N)
-{
-
-    for (int step = 0; step < N - 1; ++step)
+    else if (N==1)
     {
-
-
-        for (int i = 0; i < N - step - 1; ++i)
+        return a;
+    }
+    else
+    {
+        for(int i=0; i<=N; i++)
         {
-
-            if (sumering(B[i],N) < sumering(B[i+1],N))
-            {
-
-                int *temp = B[i];
-                B[i] = B[i+1];
-                B[i+1] = temp;
-
-            }
+            value*=a;
         }
     }
-    printMatrix(B,N);
-
+    return value;
 }
-
-void printMatrix(int **matrix,int N)
-{
-    for (i=0; i<N; i++)
-    {
-        for(j=0; j<N; j++)
-        {
-            printf("%d     ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
